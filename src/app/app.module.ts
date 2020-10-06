59f810806656f8585883485e3df6f003ed1641d1
levanlongg <levanlong99pchy@gmail.com>
2020-10-07 02:00:24 +0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { Er404Component } from './shared/er404/er404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    Er404Component,
   ],
   imports: [
     BrowserModule

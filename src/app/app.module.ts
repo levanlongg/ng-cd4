c163b230bae4d69c2278673ec4d071a2772925f6
levanlongg <levanlong99pchy@gmail.com>
2020-10-09 12:53:11 +0700

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,25 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { Er404Component } from './shared/er404/er404.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'er404',
+    component:Er404Component
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  { path: '**', component:Er404Component}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +36,10 @@ import { Er404Component } from './shared/er404/er404.component';
     Er404Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, { useHash: true })  // .../#/crisis-center/
   ],
   providers: [],
   bootstrap: [AppComponent]
